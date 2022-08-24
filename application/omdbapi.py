56b0ac6091a5fa0d5954065e775a75400069d94a
Bastien Orivel <eijebong@bananium.fr>
2022-08-25 00:55:59 +0200

    Try multiple times to guess a movie with different info
    First try with "$title + ' ' + $subtitle". If not found, check if the
    year is at the end of it and try again. Sometimes guessit fails to see
    the year at the end of a title. Then fall back to the original behavior
    which was only using the title.

--- application/omdbapi.py
+++ application/omdbapi.py
@@ -4,6 +4,7 @@ import os
 
 import aiohttp
 import guessit
+import re
 
 from application.models import Movie, NewMovieNotification, MovieDirectory
 from django.conf import settings
@@ -25,6 +26,7 @@ async def save_poster(poster_url, loop, aiohttp_session):
         return os.path.join("posters", filename)  # return media url
 
 
+END_WITH_YEAR_RE = re.compile(r"(.*) ([1-2][0-9]{3})$")
 class OMDBAPI:
     def __init__(self, loop, aiohttp_session):
         self.loop = loop
@@ -34,12 +36,35 @@ class OMDBAPI:
         infos = guessit.guessit(name, {"type": "movie"})
         if not infos.get('title'):
             return
-        try:
-            params = urlencode({'s': infos['title'] + infos.get("alternative_title", ""), 'y': infos['year'], 'type': 'movie', 'r': 'json', 'apikey': settings.OMDB_API_KEY})
-        except KeyError:
-            params = urlencode({'s': infos['title'] + infos.get("alternative_title", ""), 'type': 'movie', 'r': 'json', 'apikey': settings.OMDB_API_KEY})
-        url = 'http://www.omdbapi.com/?%s' % params
 
+        params = {'s': infos['title'], 'type': 'movie', 'r': 'json', 'apikey': settings.OMDB_API_KEY}
+        if "year" in infos:
+            params['y'] = infos["year"]
+
+        try_again_on_fail = False
+        if 'alternative_title' in infos:
+            params['s'] += " " + infos["alternative_title"]
+            try_again_on_fail = True
+
+        movie = await self.try_search(params)
+
+        if movie is None and "y" not in params:
+            m = END_WITH_YEAR_RE.search(params['s'])
+            if m:
+                params["s"] = m.group(1)
+                params["y"] = m.group(2)
+                movie = await self.try_search(params)
+
+        if movie is None and try_again_on_fail:
+            params['s'] = infos['title']
+            movie = await self.try_search(params)
+
+        return movie
+
+
+    async def try_search(self, options):
+        params = urlencode(options)
+        url = 'http://www.omdbapi.com/?%s' % params
         async with self.aiohttp_session.get(url) as resp:
             data = await resp.text()
             resp = json.loads(data)
@@ -51,6 +76,7 @@ class OMDBAPI:
                         imdbid=res['imdbID'],
                         poster=await save_poster(poster, self.loop, self.aiohttp_session),
                     )
+        return None
 
     async def get_detailled_infos(self, imdbid):
         params = urlencode({'i': imdbid, 'plot': 'full', 'r': 'json', 'apikey': settings.OMDB_API_KEY})
